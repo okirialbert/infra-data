e98135cd6438ab7e914d31e375629c6deeafb29a
Albert Olweny <41315252+okirialbert@users.noreply.github.com>
2023-08-25 10:17:10 +0300

    transforming and store oecd data

--- oecd_get.py
+++ oecd_get.py
@@ -3,17 +3,21 @@ from __future__ import annotations
 
 import os
 from datetime import datetime
+import pandas as pd
+import re
+import json
+import tempfile
+from pathlib import Path
 
 from airflow import DAG
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
 
 from airflow.decorators import dag
 
-import pandas as pd
-import re
-import json
+
 
 ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
 DAG_ID = "OECD_get_store"
@@ -75,13 +79,39 @@ def transform_response(**kwargs):
         obs['unit_measure'] = obs.apply(lambda x: unitMeasureList[int(x['dimensions'][3])]['id'], axis = 1)
         obs['time'] = obs.apply(lambda x: timeList[int(x['dimensions'][5])]['id'], axis = 1)
         obs['names'] = obs['subject'] + '_' + obs['measure']
+        obs = obs.reset_index()
+        obs = obs[['series', 'ref_area', 'frequency', 'subject', 'measure', 'unit_measure', 'time']]
+        obs = obs.astype({'series': 'float32', 'ref_area': 'string', 'frequency': 'string', 'subject': 'string', 'measure': 'string', 'unit_measure': 'string'})
+        obs['time']= pd.to_datetime(obs['time'], format='%Y')
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            tmp_path = os.path.join(tmp_dir, "obs.csv")
+            obs.to_csv("obs.csv",header=None, index=False)
+
+        ti.xcom_push(key="store_path", value=tmp_path)
 
             # data = obs.pivot_table(index = ['ref_area', 'unit_measure'], columns = ['time'], values = 'series')
             
-        return (obs)
+        return obs
+    
+def read_store(**kwargs):
+    ti = kwargs["ti"]
+    path = ti.xcom_pull(key="store_path", task_ids='transform_response_to_df')
+    pg_hook = PostgresHook(postgres_conn_id="postgres_default")
+    store = pg_hook.bulk_load("green_table", path)
+    
+    return store
+
 
 transform_task = PythonOperator(
    task_id="transform_response_to_df",
+   provide_context=True,
+   python_callable=transform_response,
+   dag=dag,
+)
+
+read_store_task = PythonOperator(
+   task_id="read_store",
    provide_context=True,
    python_callable=transform_response,
    dag=dag,
@@ -91,16 +121,32 @@ create_table = PostgresOperator(
         task_id="create_oecd_table",
         postgres_conn_id="postgres_default",
         sql="""
-            CREATE TABLE IF NOT EXISTS green (
-            obs_id SERIAL PRIMARY KEY,
+            CREATE TABLE IF NOT EXISTS green_table (
             series NUMERIC NOT NULL,
             ref_area VARCHAR NOT NULL,
-            time DATE NOT NULL,
-            frequency VARCHAR NOT NULL
-            measure VARCHAR NOT NULL
-            unit_measure VARCHAR NOT NULL
-            subject VARCHAR NOT NULL);
+            frequency VARCHAR NOT NULL,
+            subject VARCHAR NOT NULL,
+            measure VARCHAR NOT NULL,
+            unit_measure VARCHAR NOT NULL,
+            time DATE NOT NULL
+            );
           """,
     )
 
-task_get_data_oecd >> transform_task >> create_table
+# populate_table = PostgresOperator(
+#         task_id="populate_table",
+#         # sql="""
+#         #     INSERT INTO green_table ( series, ref_area, frequency, subject, measure, unit_measure, time )
+#         #     VALUES
+#         #     ({{task_instance.xcom_pull(task_ids='transform_response_to_df')}});
+#         #     """,
+#         sql = '''
+#         COPY green_table
+#         FROM '{{task_instance.xcom_pull(key='store_path', task_ids='transform_response_to_df')}}' 
+#         DELIMITER ',' CSV;
+#         '''
+#     )
+
+task_get_data_oecd >> transform_task >> create_table >> read_store_task 
+
+# >> populate_table
